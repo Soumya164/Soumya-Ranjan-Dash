bbd1896fd557b6fda0c77c1530f3c6971cd576b8
Soumya164 <74524972+Soumya164@users.noreply.github.com>
2021-08-05 00:52:00 -0400

    07/10

---
+++ QuestionNo07.js
@@ -0,0 +1,23 @@
+/*
+Q7. Create a nested array object called operatingSystem and add below key and values
+Name - Ubuntu , version - 18.4 , license - open source
+*/
+var operatingSystem = [
+{
+Name: 'Ubuntu',
+version: '18.4',
+license: 'open source'
+}];
+
+console.log(operatingSystem)
+
+/*
+OUTPUT:
+//[object Array] (1)
+[// [object Object]
+{
+  "Name": "Ubuntu",
+  "version": "18.4",
+  "license": "open source"
+}]
+*/
